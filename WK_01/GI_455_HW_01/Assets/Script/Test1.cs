6b6e7096290f78aad9ea58133d063c133b522342
PMG-20200526XGF\Administrator <nattakorn.kerd@bumail.net>
2021-01-27 12:46:02 +0700

    Send Assignment Week1

---
+++ WK_01/GI_455_HW_01/Assets/Script/Test1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Test1 : MonoBehaviour
+{
+    public InputField inputField;
+    public Text resultText;
+    //public string income;
+
+    public List<string> nameOfBrandList = new List<string>();
+
+    public void Start()
+    {
+        string nameOfBrand_1 = "Sony";
+        nameOfBrandList.Add(nameOfBrand_1);
+        string nameOfBrand_2 = "Xiaomi";
+        nameOfBrandList.Add(nameOfBrand_2);
+        string nameOfBrand_3 = "Unreal";
+        nameOfBrandList.Add(nameOfBrand_3);
+        string nameOfBrand_4 = "Unity";
+        nameOfBrandList.Add(nameOfBrand_4);
+        string nameOfBrand_5 = "Apple";
+        nameOfBrandList.Add(nameOfBrand_5);
+        string nameOfBrand_6 = "Samsung";
+        nameOfBrandList.Add(nameOfBrand_6);
+
+
+
+
+
+    }
+    public void OnFind()
+    {
+
+
+        if (nameOfBrandList.Contains(inputField.text))
+        {
+            resultText.text = "[" + inputField.text + "]" + "Has found";
+        }
+        else
+        {
+            resultText.text = "has not found";
+        }
+
+
+    }
+}
